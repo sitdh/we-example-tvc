b8a985a38bab285c56ed841106b443dfa93e2354
Sitdhibong Laokok <goto.champ@gmail.com>
2014-05-28 15:04:23 +0700

    Add controller for UITableViewController

---
+++ We-App-TVC/WTVStudentsTableViewController.h
@@ -0,0 +1,13 @@
+//
+//  WTVStudentsTableViewController.h
+//  We-App-TVC
+//
+//  Created by Sitdhibong Laokok on 28/5/57.
+//  Copyright (c) พ.ศ. 2557 App Tree. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface WTVStudentsTableViewController : UITableViewController
+
+@end
